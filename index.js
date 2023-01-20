3b66f389fae80afad3fd81ffdd64ed27f73b63a4
AyushGupta-0 <118269589+AyushGupta-0@users.noreply.github.com>
2023-01-20 19:23:37 +0530

    queryslectorall

--- index.js
+++ index.js
@@ -19,14 +19,30 @@
 
 // }
 // get eliments by tag name
-let li = document.getElementsByTagName('li')
-console.log(li[3])
-console.log(li)
-li[2].style.backgroundColor ='green'
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight='bold'
+// let li = document.getElementsByTagName('li')
+// console.log(li[3])
+// console.log(li)
+// li[2].style.backgroundColor ='green'
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold'
 
-}
-let x= document.getElementsByClassName('notsame')
+// }
+// let x= document.getElementsByClassName('notsame')
+// console.log(x)
+// // x[0].style.border='dotted 3px red'
+// let x = document.querySelector('#main-header')
+// x.style.borderBottom='solid 10px yellow'
+// let input = document.querySelector('input[type="submit"]')
+// input.value = 'send'
+// let seconditem = document.querySelector('.list-group-item:nth-child(2)')
+// seconditem.style.backgroundColor='green'
+// let thirditem = document.querySelector('.list-group-item:nth-child(3)')
+// thirditem.style = 'display:none'
+let x= document.querySelectorAll('li')
 console.log(x)
-x[0].style.border='dotted 3px red'
+x[1].style.color='green'
+let odd = document.querySelectorAll('li:nth-child(odd)')
+for(var i =0;i<odd.length;i++){
+    odd[i].style.backgroundColor ='green'
+}
+

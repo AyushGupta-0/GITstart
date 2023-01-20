9f181688d92440c0a2380c984a9a4c048aef360b
AyushGupta-0 <118269589+AyushGupta-0@users.noreply.github.com>
2023-01-20 18:37:27 +0530

    4th

--- index.js
+++ index.js
@@ -10,11 +10,23 @@
 // // console.log(x)
 // x.style.border='dotted 3px red'
 // y.style.borderBottom='solid 10px green'
-let items = document.getElementsByClassName('list-group-item')
-// console.log(items)
-console.log(items[2])
-items[2].style.backgroundColor ='green'
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
+// let items = document.getElementsByClassName('list-group-item')
+// // console.log(items)
+// console.log(items[2])
+// items[2].style.backgroundColor ='green'
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold'
+
+// }
+// get eliments by tag name
+let li = document.getElementsByTagName('li')
+console.log(li[3])
+console.log(li)
+li[2].style.backgroundColor ='green'
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold'
 
 }
+let x= document.getElementsByClassName('notsame')
+console.log(x)
+x[0].style.border='dotted 3px red'

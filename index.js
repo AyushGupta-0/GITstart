f166a0abea349cf356b2b9e43370db475d8c3698
AyushGupta-0 <118269589+AyushGupta-0@users.noreply.github.com>
2023-01-21 13:58:27 +0530

    6th

--- index.js
+++ index.js
@@ -38,11 +38,49 @@
 // seconditem.style.backgroundColor='green'
 // let thirditem = document.querySelector('.list-group-item:nth-child(3)')
 // thirditem.style = 'display:none'
-let x= document.querySelectorAll('li')
-console.log(x)
-x[1].style.color='green'
-let odd = document.querySelectorAll('li:nth-child(odd)')
-for(var i =0;i<odd.length;i++){
-    odd[i].style.backgroundColor ='green'
-}
+// let x= document.querySelectorAll('li')
+// console.log(x)
+// x[1].style.color='green'
+// // let odd = document.querySelectorAll('li:nth-child(odd)')
+// // for(var i =0;i<odd.length;i++){
+// //     odd[i].style.backgroundColor ='green'
+// // }
+// let itemlist = document.querySelector('#items')
+// // console.log(itemlist.parentNode)
+// // itemlist.parentNode.style.border = 'dashed 5px yellow '
+
+// // console.log(itemlist.childNodes)
+// // itemlist.childNode.style.border = 'dashed 5px yellow '
+// // console.log(itemlist.children)
+// // console.log(itemlist.children[2])
+// itemlist.children[2].style.color = 'green'
+// // firstchild 
+// console.log(itemlist.firstElementChild)
+let newdiv = document.createElement('div')
+//add class
+newdiv.className='hello'
+//add id
+newdiv.id='hello1';
+//add attribute
+newdiv.setAttribute('title','hello div')
+//create a new node
+let newdivtext = document.createTextNode('HEllo')
+//add text to div
+newdiv.appendChild(newdivtext)
+let container = document.querySelector('header .container')
+let h1 = document.querySelector('header h1' )
+container.insertBefore(newdiv,h1)
+
+console.log(newdiv)
+let newitem = document.createElement('div')
+newitem.className='listitem'
+newitem.id ='listitem1'
+newitem.setAttribute('title','helloitem')
+console.log(newitem)
+let newitemtext = document.createTextNode('HEllo')
+newitem.appendChild(newitemtext)
+let container1 = document.querySelector('ul .list-group')
+let h2 = document.querySelector('ul li' )
+container1.insertBefore(newitem, h2)
+
 

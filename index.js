3a1269a6a150ad5d4ace161b8e0505e4d9815632
AyushGupta-0 <118269589+AyushGupta-0@users.noreply.github.com>
2023-01-21 19:40:43 +0530

    adding another search + filter

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var form = document.getElementById('addForm')
 var itemlist = document.getElementById('items')
+var filter = document.getElementById('filter');
 form.addEventListener('submit',additem);
 itemlist.addEventListener('click',removeItem);
+filter.addEventListener('keyup',filterItems)
 function additem(e){
     e.preventDefault();
     var newitem = document.getElementById('item').value;
+    var description = document.getElementById('description').value;
     var li = document.createElement('li');
     li.className='list-group-item';
-    console.log(li)
+    var desnode = document.createTextNode(" "+ description)
     li.appendChild(document.createTextNode(newitem));
+    li.appendChild(desnode)
     // delete button starts 
     var deleteBtn = document.createElement('button');
     deleteBtn.className='btn btn-danger btn-sm float-right delete'
@@ -30,6 +34,17 @@ function removeItem(e){
         itemlist.removeChild(li);
       }
     }
+}
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    var items = itemlist.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
   }
-  
 
